ac74bdcb8414f304e14f44b1d4c3ef161f2e2cfb
Rifazul <rifazul50@gmail.com>
2023-11-05 22:25:28 +0600

    Add A Specific Id data load api

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rwemj7d.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -45,6 +45,14 @@ async function run() {
     }
    const result = await jobsCollection.find(categoryItems).toArray();
    res.send(result)
+   })
+
+   // Specific ID dara Data Load
+   app.get("/api/v1/jobs/:id", async(req,res)=>{
+       const id = req.params.id;
+       const query = {_id : new ObjectId(id)};
+       const result = await jobsCollection.findOne(query);
+       res.send(result)
    })
 
    // post method in jobs collection
